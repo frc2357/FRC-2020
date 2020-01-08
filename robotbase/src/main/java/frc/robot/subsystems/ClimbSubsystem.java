72d0bd457d4249f196940da08434c925d232ce18
Jbrn-2357 <53975911+Jbrn-2357@users.noreply.github.com>
2020-01-07 20:01:32 -0600

    addClimbSubStructure
    Began setting of structure of ClimbSub.

---
+++ robotbase/src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.sensors.PigeonIMU;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClimbSubsystem extends SubsystemBase {
+    
+    private WPI_TalonSRX m_leftReelTalon;
+    private WPI_TalonSRX m_rightReelTalon;
+
+    private PigeonIMU m_gyro;
+
+    public ClimbSubsystem(int talonRightID,int talonLeftID, int gyroID) {
+
+        m_leftReelTalon = new WPI_TalonSRX(talonLeftID);
+        m_rightReelTalon = new WPI_TalonSRX(talonRightID);
+        m_gyro = new PigeonIMU(gyroID);
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+    //rename position to what unit it is
+    public void runMotor(WPI_TalonSRX reelTalon, double position) {
+        reelTalon.set(ControlMode.Position, position);
+    }
+}
