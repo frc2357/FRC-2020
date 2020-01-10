b53d10dd030a881ab0c1ac7b4350d391d2df01ff
NC2357 <crazyfloob@gmail.com>
2020-01-09 19:37:09 -0600

    Began TurretSubsystem

---
+++ robotbase/src/main/java/com/systemmeltdown/robot/subsystems/TurretSubsystem.java
@@ -0,0 +1,26 @@
+package com.systemmeltdown.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TurretSubsystem extends SubsystemBase {
+    private WPI_TalonSRX m_rotateMotor;
+
+    public TurretSubsystem(int rotateMotorID) {
+
+        m_rotateMotor = new WPI_TalonSRX(rotateMotorID);
+    }
+
+    @Override
+    public void periodic() {
+      // This method will be called once per scheduler run
+    }
+
+    public void runMotor(WPI_TalonSRX rotatorMotor) {
+        rotatorMotor.set(ControlMode.Position);
+    }
+
+
+}
