5a5e1aa9c9ed533199db5d882c55da5aa8d177b3
GameMagma <53975931+GameMagma@users.noreply.github.com>
2020-01-12 14:15:43 -0600

    Deleted Excess Whitespace
    There were lots of whitespace lines that did not need to be there, so i deleted them.

--- robotbase/src/main/java/com/systemmeltdown/robot/subsystems/IntakeSub.java
+++ robotbase/src/main/java/com/systemmeltdown/robot/subsystems/IntakeSub.java
@@ -2,21 +2,17 @@ package com.systemmeltdown.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class IntakeSub extends SubsystemBase {
-
     private Solenoid m_intakeSolenoid;
     private WPI_TalonSRX m_intakeTalon;
     private boolean m_isArmUp = true;
 
     public IntakeSub(int channel, int intakeTalonID) {
-
         m_intakeSolenoid = new Solenoid(channel);
         m_intakeTalon = new WPI_TalonSRX(intakeTalonID);
- 
     }
 
     @Override
@@ -24,24 +20,21 @@ public class IntakeSub extends SubsystemBase {
         // nothing
     }
 
-    //percentPowerOuput defintion: -1 = reverse | 0 = stop | 1 = foward
+    // percentPowerOuput defintion: -1 = reverse | 0 = stop | 1 = foward
     public void triggerIntakeRoller(double percentPowerOutput) {
-        m_intakeTalon.set(ControlMode.PercentOutput, percentPowerOutput);   
+        m_intakeTalon.set(ControlMode.PercentOutput, percentPowerOutput);
     }
 
-    //Current values are most likley incorrect, actual values will be figured out through testing.
+    // Current values are most likley incorrect, actual values will be figured out
+    // through testing.
     public void changeArmPosition() {
 
-        if(m_isArmUp) {
+        if (m_isArmUp) {
             m_intakeSolenoid.set(false);
             m_isArmUp = false;
-
         } else {
             m_intakeSolenoid.set(true);
             m_isArmUp = true;
         }
-        
     }
-
-    
 }
