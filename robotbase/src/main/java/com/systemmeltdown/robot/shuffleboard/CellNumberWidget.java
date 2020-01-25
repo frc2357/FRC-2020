21ee81876f00cf44df494e98c0b488d1104ab970
GameMagma <53975931+GameMagma@users.noreply.github.com>
2020-01-25 13:45:41 -0600

    Fully created cellNumberWidget
    Created the Cell Number Widget, it should work now. I have not been able to test it yet, but it should work.

--- robotbase/src/main/java/com/systemmeltdown/robot/shuffleboard/CellNumberWidget.java
+++ robotbase/src/main/java/com/systemmeltdown/robot/shuffleboard/CellNumberWidget.java
@@ -3,22 +3,48 @@ package com.systemmeltdown.robot.shuffleboard;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
+/**
+* A Shuffleboard widget that displays the amount of power cells in the robot
+* 
+* @param tabTitle Title of the tab you want to add the widget to
+*/
 class CellNumberWidget {
     private static final String TITLE = "Num of Power Cells";
-    int m_cellNum = 0;
+    private static int m_cellNum = 0;
+    private static NetworkTableEntry m_cellNumWidget;
 
-    /**
-     * A Shuffleboard widget that displays the amount of power cells in the robot
-     * 
-     * @param tabTitle Title of the tab you want to add the widget to
-     */
+    
     public CellNumberWidget(String tabTitle) {
         NetworkTableEntry cellNumWidget = Shuffleboard.getTab(tabTitle)
             .add("Num of Cells", 0)
             .withWidget(BuiltInWidgets.kTextView)
             .getEntry();
+
+        m_cellNumWidget = cellNumWidget;
+  }
+
+  /**
+   * Adds one to the power cell count.
+   */
+  public void addBall() {
+      m_cellNum++;
+      m_cellNumWidget.setNumber(m_cellNum);
+  }
+
+  /**
+   * Subtracts one from the power cell count.
+   */
+  public void subBall() {
+      m_cellNum--;
+      m_cellNumWidget.setNumber(m_cellNum);
+  }
+
+  /**
+   * @return int: Amount of Power Cells in the robot.
+   */
+  public int getBallCount() {
+      return m_cellNum;
   }
 
   public void show() {
