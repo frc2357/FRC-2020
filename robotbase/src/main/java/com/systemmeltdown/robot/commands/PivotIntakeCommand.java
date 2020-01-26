6dc67cd814cc9092e4b27d55ce4d4a061ef8b86b
NC2357 <crazyfloob@gmail.com>
2020-01-26 14:29:30 -0600

    created PivotIntakeCommand

---
+++ robotbase/src/main/java/com/systemmeltdown/robot/commands/PivotIntakeCommand.java
@@ -0,0 +1,24 @@
+package com.systemmeltdown.robot.commands;
+
+import com.systemmeltdown.robot.subsystems.IntakeSub;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class PivotIntakeCommand extends CommandBase {
+    private IntakeSub m_intakeSubsystem;
+
+    public PivotIntakeCommand(IntakeSub intakeSubsystem) {
+        m_intakeSubsystem = intakeSubsystem;
+        addRequirements(m_intakeSubsystem);
+    }
+
+    @Override 
+    public void initialize() {
+        m_intakeSubsystem.changeArmPosition();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    } 
+}
