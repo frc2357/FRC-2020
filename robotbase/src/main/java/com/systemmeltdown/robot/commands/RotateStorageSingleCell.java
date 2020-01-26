59e370d9264348bf6d5f902dce14d9950f3aafad
Marjie Hensz <marjie.hensz@gmail.com>
2020-01-26 14:47:41 -0600

    Added stubs for commands to rotate the storage system continuously and a single cell.  More will need to be added when the subsystem is updated.

---
+++ robotbase/src/main/java/com/systemmeltdown/robot/commands/RotateStorageSingleCell.java
@@ -0,0 +1,21 @@
+package com.systemmeltdown.robot.commands;
+import com.systemmeltdown.robot.subsystems.StorageSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/*
+* This command will rotate the carousel one cell for single cell shooting
+*/
+
+public class RotateStorageSingleCell extends CommandBase {
+    private StorageSubsystem m_storageSubsystem;
+
+    public RotateStorageSingleCell (StorageSubsystem storageSubsystem) {
+        m_storageSubsystem = storageSubsystem;
+        addRequirements(m_storageSubsystem);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+}
