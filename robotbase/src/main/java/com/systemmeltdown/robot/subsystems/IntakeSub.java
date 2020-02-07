364708c47e848267a04169e5f0e06c745aa99250
GameMagma <53975931+GameMagma@users.noreply.github.com>
2020-02-06 18:57:35 -0600

    Reimporting unused imports per stick's request

--- robotbase/src/main/java/com/systemmeltdown/robot/subsystems/IntakeSub.java
+++ robotbase/src/main/java/com/systemmeltdown/robot/subsystems/IntakeSub.java
@@ -4,8 +4,11 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.systemmeltdown.robotlib.subsystems.ClosedLoopSubsystem;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+// import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
 public class IntakeSub extends ClosedLoopSubsystem {
-    //private Solenoid m_intakeSolenoid;
+    // private DoubleSolenoid m_intakeSolenoid;
     private WPI_TalonSRX m_intakeTalon;
     private boolean m_rollIntoBot = true;
 
@@ -28,15 +31,15 @@ public class IntakeSub extends ClosedLoopSubsystem {
         m_rollIntoBot = !m_rollIntoBot;
     }
 
-    public void changeArmPosition() {
-        // switch (m_intakeSolenoid.get()) {
-        // case kForward: {
-        //     m_intakeSolenoid.set(Value.kReverse);
-        //     break;
-        // }
-        // default: {
-        //     m_intakeSolenoid.set(Value.kForward);
-        // }
-        // }
-    }
+    // public void changeArmPosition() {
+    //     switch (m_intakeSolenoid.get()) {
+    //         case kForward: {
+    //             m_intakeSolenoid.set(Value.kReverse);
+    //             break;
+    //         } 
+    //         default: {
+    //             m_intakeSolenoid.set(Value.kForward);
+    //         }
+    //     }
+    // }
 }
