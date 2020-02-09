4f6b918cc4dacd58f150ee239c90c9896452f86e
GameMagma <53975931+GameMagma@users.noreply.github.com>
2020-02-09 14:54:04 -0600

    Javadocs for classes in the controls folder done

--- robotbase/src/main/java/com/systemmeltdown/robot/controls/GunnerControls.java
+++ robotbase/src/main/java/com/systemmeltdown/robot/controls/GunnerControls.java
@@ -7,6 +7,11 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
+/**
+ * These are the controls for the gunner.
+ * 
+ * @category Drive
+ */
 public class GunnerControls {
     XboxController m_controller;
 
@@ -14,6 +19,9 @@ public class GunnerControls {
     public AxisThresholdTrigger m_leftTrigger;
     public JoystickButton m_yButton;
 
+    /**
+     * @param controller The gunner's {@link XboxController}.
+     */
     public GunnerControls(XboxController controller) {
         m_controller = controller;
         m_rightTrigger = new AxisThresholdTrigger(controller, Hand.kRight, .1);
@@ -21,6 +29,13 @@ public class GunnerControls {
         m_yButton = new JoystickButton(controller, XboxRaw.Y.value);
     }
 
+    /**
+     * Gets the current trigger value from the hand on the left or right.
+     * 
+     * @param hand Which hand you want to get a value from.
+     * 
+     * @return The trigger value from the left or right hand.
+     */
     public double getTriggerValue(Hand hand) {
         return m_controller.getTriggerAxis(hand);
     }
