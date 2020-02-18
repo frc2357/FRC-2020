99e3164c33e57d4fd27dbe93359a69bfdf273782
GameMagma <53975931+GameMagma@users.noreply.github.com>
2020-02-18 17:07:35 -0600

    Remove unused import

--- robotbase/src/main/java/com/systemmeltdown/robot/commands/AutoCenterOnBall.java
+++ robotbase/src/main/java/com/systemmeltdown/robot/commands/AutoCenterOnBall.java
@@ -1,8 +1,6 @@
 package com.systemmeltdown.robot.commands;
 
 import com.systemmeltdown.robot.subsystems.StorageSubsystem;
-import com.systemmeltdown.robot.subsystems.StorageSubsystem.Configuration;
-
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
