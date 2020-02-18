c68590d6f345dfa0c589114e1e8a3e0a510df7f3
GameMagma <53975931+GameMagma@users.noreply.github.com>
2020-02-18 17:16:56 -0600

    Remove useless comment and add Java Doc

--- robotbase/src/main/java/com/systemmeltdown/robot/commands/AutoCenterOnBall.java
+++ robotbase/src/main/java/com/systemmeltdown/robot/commands/AutoCenterOnBall.java
@@ -5,12 +5,23 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 
+/**
+ * A command that should automatically run at the beginning of every match.
+ * This command will rotate the carousel until a ball is detected by the 
+ * infrared sensor in the storage. When a ball is detected, the Encoder in
+ * the storage will be reset so that it centers on the point the carousel
+ * was stopped at.
+ */
 class AutoCenterOnBall extends CommandBase {
-    // I don't know what port the sensor is being plugged into, so I have to do this for now.
     private DigitalInput m_infraredSensor;
-    
     private StorageSubsystem m_storageSub;
 
+    /**
+     * @param storageSub The {@link storageSub}. Needed so this command can rotate
+     *                   the carousel.
+     * 
+     * @param port       The port the infrared sensor in the storage is plugged into.
+     */
     public AutoCenterOnBall(StorageSubsystem storageSub, int port) {
         m_infraredSensor = new DigitalInput(port);
 
