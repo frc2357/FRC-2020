26c916eaa0c865ba9a2726119cf2d92a7c5e967e
GameMagma <53975931+GameMagma@users.noreply.github.com>
2020-02-22 13:57:30 -0600

    Implement RobotLog into feederSubLog

--- robotbase/src/main/java/com/systemmeltdown/robot/subsystems/FeederSubsystem.java
+++ robotbase/src/main/java/com/systemmeltdown/robot/subsystems/FeederSubsystem.java
@@ -4,7 +4,6 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.systemmeltdown.robotlib.subsystems.ClosedLoopSubsystem;
 import com.systemmeltdown.robotlog.topics.DoubleTopic;
-
 import edu.wpi.first.wpilibj.DigitalInput;
 
 /**
@@ -19,8 +18,14 @@ public class FeederSubsystem extends ClosedLoopSubsystem {
     private WPI_TalonSRX m_feederMotor;
     private DigitalInput m_feedSensor;
 
-    private final DoubleTopic motorCurrentTopic = new DoubleTopic("Feeder Motor Current", 0.25);
     
+
+    /* RobotLog Topics */
+    //private final StringTopic m_feederSubErrorTopic = new StringTopic("Feeder Sub Error");
+    // /\ Unused /\
+    private final DoubleTopic m_motorCurrentTopic = new DoubleTopic("Feeder Motor Current", 0.25);
+    private final DoubleTopic m_feederMotorSpeedTopic = new DoubleTopic("Feeder Motor Speed Set", 0.25);
+
     /**
      * @param feederMotorID The ID of the feeder motor.
      * @param feederSensorID The ID of the infrared sensor that detects if there is a cell in the slot before the feeder
@@ -35,7 +40,7 @@ public class FeederSubsystem extends ClosedLoopSubsystem {
 
     @Override
     public void periodic() {
-        motorCurrentTopic.log(m_feederMotor.getStatorCurrent());
+        m_motorCurrentTopic.log(m_feederMotor.getStatorCurrent());
     }
 
     /**
@@ -44,6 +49,7 @@ public class FeederSubsystem extends ClosedLoopSubsystem {
      */
     public void runFeederMotor(double speed) {
         m_feederMotor.set(ControlMode.PercentOutput, speed);
+        m_feederMotorSpeedTopic.log(speed);
     }
 
     public boolean isFeedSensorBlocked() {
