c09559f7eeefb938646352be67dc38c1cc63f5ba
Jbrn <jameson.brown627@raypec.org>
2020-02-23 17:06:03 -0600

    Add semi auto commands

---
+++ robotbase/src/main/java/com/systemmeltdown/robot/commands/AutoIntake.java
@@ -0,0 +1,40 @@
+package com.systemmeltdown.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import com.systemmeltdown.robot.subsystems.FeederSubsystem;
+import com.systemmeltdown.robot.subsystems.IntakeSubsystem;
+import com.systemmeltdown.robot.subsystems.StorageSubsystem;
+
+public class AutoIntake extends SequentialCommandGroup {
+    public class countTillFive extends CommandBase {
+        StorageSubsystem m_storageSub;
+        public countTillFive(StorageSubsystem storageSub) {
+            m_storageSub = storageSub;
+
+            addRequirements(storageSub);
+
+        }
+        @Override
+        public boolean isFinished() {
+            return(m_storageSub.getNumOfCells() >= 5);
+        }
+    }
+   IntakePickupWithoutController m_pickupWithout;
+
+    public AutoIntake(IntakeSubsystem intakeSub, StorageSubsystem storageSub, FeederSubsystem feederSub) {
+        addRequirements(intakeSub);
+        addCommands(new IntakePickupWithoutController(intakeSub));
+
+        addCommands(new ParallelDeadlineGroup(new countTillFive(storageSub), new RecountCellsCommand(storageSub, feederSub)));
+        
+
+        // To be finished once recount cell is tested
+
+    }
+
+   
+    
+}
---
+++ robotbase/src/main/java/com/systemmeltdown/robot/commands/AutoShoot.java
@@ -0,0 +1,45 @@
+package com.systemmeltdown.robot.commands;
+
+import com.systemmeltdown.robot.subsystems.FeederSubsystem;
+import com.systemmeltdown.robot.subsystems.ShooterSubsystem;
+import com.systemmeltdown.robot.subsystems.StorageSubsystem;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class AutoShoot extends SequentialCommandGroup {
+    public class RotateBetween extends CommandBase {
+        StorageSubsystem m_storageSubsystem;
+        public RotateBetween(StorageSubsystem storageSubsystem) {
+
+            m_storageSubsystem = storageSubsystem;
+            addRequirements(storageSubsystem);
+            
+        }
+
+        @Override
+        public boolean isFinished() {
+            return(m_storageSubsystem.getEncoderValue() == 0.1);
+        }
+    }
+
+    public AutoShoot(StorageSubsystem storageSub, FeederSubsystem feederSub, ShooterSubsystem shooterSub) {
+        ParallelCommandGroup startUpCommandGroup = new ParallelCommandGroup(
+            new SequentialCommandGroup(
+                new ParallelDeadlineGroup(new RotateBetween(storageSub), new RotateStorageContinuous(storageSub)),
+                new FeedToShooterCommand(feederSub)
+            ),
+            new ShootCommand(shooterSub)
+        );
+        ParallelCommandGroup fireCommandGroup = new ParallelCommandGroup(
+            new FeedToShooterCommand(feederSub),
+            new ShootCommand(shooterSub),
+            new RotateStorageContinuous(storageSub)
+        );
+
+        addCommands(startUpCommandGroup, fireCommandGroup);
+        
+    }
+}
